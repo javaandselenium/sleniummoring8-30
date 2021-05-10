a72f8757922affa5d8128f99b4d1c6afc99401e8
QSP <QSP@QSP>
2021-05-10 10:25:28 +0530

    Popups

--- src/basics/Test3.java
+++ src/basics/Test3.java
@@ -7,7 +7,8 @@ public class Test3 {
 
 	public static void main(String[] args) throws InterruptedException {
 	WebDriver driver=new ChromeDriver();
-	driver.get("https://www.myntra.com/");
+	driver.navigate().to("https://www.myntra.com/");
+	
 	Thread.sleep(3000);
 	driver.navigate().back();
 	Thread.sleep(3000);
